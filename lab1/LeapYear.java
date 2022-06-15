2cbd6822296d157e4be7300db8c3c2fd2df47759
wgywmm <wgywmm@gmail.com>
2022-06-15 10:55:22 +0800

    commit lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -3,6 +3,13 @@
  */
 public class LeapYear {
 
+    /**
+     * @param year to be analyzed
+     */
+    public static boolean isLeapYear(int year) {
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+    }
+
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
